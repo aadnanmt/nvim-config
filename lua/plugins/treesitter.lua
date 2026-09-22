96ef38bd18d3e9e1cadc490e2ad84f32aa75ed70
Adnan Slamet Wibowo <adnan@nanoolabs.dev>
2026-09-22 16:05:03 +0700

    feat: add treesitter parsers (bash, json, yaml, html, css, astro)

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "json",
+        "jsonc",
+        "yaml",
+        "html",
+        "css",
+        "astro",
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+      },
+    },
+  },
+}
