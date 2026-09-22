3eba2faee784273684f43e001a9c784b3b9ad36d
Adnan Slamet Wibowo <adnan@nanoolabs.dev>
2026-09-22 14:32:03 +0700

    feat: add plugin specs (surround, telescope-fzf, grug-far, markdown, render-markdown)

---
+++ lua/plugins/telescope-fzf.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    lazy = true,
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+}
